8dd89e834397ff00e7955925b2caf59f7b5bb307
kumardevnotes <kumar.thoudishetti@gmail.com>
2025-10-11 22:15:41 +0530

    Java file io examples added

---
+++ src/test/java/com/java/file/io/ReadPropertiesFileDemo.java
@@ -0,0 +1,23 @@
+package com.java.file.io;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+
+public class ReadPropertiesFileDemo {
+
+	public static void main(String[] args) throws Exception {
+		FileReader file = new FileReader("./src//test//resources//App.properties");
+		Properties props =  new Properties();
+		props.load(file);
+		System.out.println(props.get("AppURL"));
+		System.out.println(props.get("AppBrowser"));
+		System.out.println(props.get("AppUserName"));
+		System.out.println(props.get("AppPassword"));
+		
+		System.out.println(props.get("AppPassword_2")); //It prints null as the key is not there in the properties document
+	}
+
+}
---
+++ src/test/java/com/java/file/io/ReadWriteTextDoc.java
@@ -0,0 +1,27 @@
+package com.java.file.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ReadWriteTextDoc {
+
+	public static void main(String[] args) {
+
+		//To load the file into Java program
+		File file = new File("./src//test//resources//Assignments.txt");
+
+		try {
+			//To read file data into BufferedReader object
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (Exception e) {
+			System.out.println("Caught an exception-->" + e);
+		}
+	}
+
+}
