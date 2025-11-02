3db5f159a160b91ab7b192d0f5168b08e6b6acaf
kumardevnotes <kumar.thoudishetti@gmail.com>
2025-11-02 21:16:07 +0530

    added TC details

--- src/test/java/com/testng/demo/BrowserSpecificDemo.java
+++ src/test/java/com/testng/demo/BrowserSpecificDemo.java
@@ -3,6 +3,7 @@ package com.testng.demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,6 +13,16 @@ public class BrowserSpecificDemo {
 	public void launchAppInChrome() throws Exception {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.speaklanguages.com/");
+		driver.quit();
+		
+	}
+	
+	@Test 
+	public void launchAppInEdge() throws Exception {
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.speaklanguages.com/");
 		driver.quit();
